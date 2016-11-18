036dcbf24d9caee495cbd9d970184e113ff5dd58
Paolo Chiodi <chiodi84@gmail.com>
2016-11-18 13:12:39 +0100

    add list of traces, help panel

--- dashboard.js
+++ dashboard.js
@@ -9,6 +9,8 @@ const Memory = require('./lib/widgets/memory')
 const MsgGraph = require('./lib/widgets/msg-graph')
 const MsgTop = require('./lib/widgets/msg-top')
 const Trace = require('./lib/widgets/trace')
+const TraceList = require('./lib/widgets/trace-list')
+const Help = require('./lib/widgets/help')
 
 const bus = new Bus()
 const collector = new Collector(bus)
@@ -27,18 +29,54 @@ function setupScreen () {
 
   screen.render()
 
+  bus.on('msg.select', showTraceList)
+  bus.on('trace.select', showTrace)
+
   const grid = new Contrib.grid({rows: 12, cols: 12, screen: screen})
   const msgGraphParent = grid.set(4, 2, 4, 5, Blessed.box, {label: 'Message Flow'})
   const msgTopParent = grid.set(0, 2, 4, 5, Blessed.box, {label: 'Messages'})
-  const memParent = grid.set(0, 0, 4, 2, Blessed.box, {label: 'Memory Utilization (%)'})
-  const traceParent = grid.set(8, 0, 4, 12, Blessed.box, {label: 'Trace', bg: 'red'})
+  const memParent = grid.set(4, 0, 4, 2, Blessed.box, {label: 'Memory Utilization (%)'})
+  const traceParent = grid.set(8, 0, 4, 12, Blessed.box, {label: 'Trace'})
+  const traceListParent = grid.set(8, 0, 4, 12, Blessed.box, {label: 'Traces'})
+  const helpParent = grid.set(0, 0, 4, 2, Blessed.box, {label: 'Help'})
 
   new Memory(bus, memParent)
   new MsgGraph(bus, msgGraphParent)
   new MsgTop(bus, msgTopParent)
   new Trace(bus, traceParent)
+  new TraceList(bus, traceListParent)
+  new Help(bus, helpParent)
+
+  traceListParent.detach()
 
   msgTopParent.focus()
+
+  function showTraceList (msg) {
+    if (msg) {
+      traceParent.detach()
+      screen.append(traceListParent)
+      traceListParent.focus()
+    }
+    else {
+      traceListParent.detach()
+      screen.append(traceParent)
+      msgTopParent.focus()
+    }
+  }
+
+  function showTrace (trace) {
+    if (trace) {
+      traceListParent.detach()
+      screen.append(traceParent)
+      traceParent.focus()
+    }
+    else {
+      traceParent.detach()
+      screen.append(traceListParent)
+      traceListParent.focus()
+    }
+  }
+
 }
 
 function update () {
